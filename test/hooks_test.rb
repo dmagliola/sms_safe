3ca7e62a081f9d0356e2a06dd932d91008e5157d
Daniel Magliola <dmagliola@crystalgears.com>
2014-12-31 16:41:13 +0000

    Tests: Hook test for ActionTexter. Basically, an integration test that makes sure this works.

--- test/hooks_test.rb
+++ test/hooks_test.rb
@@ -13,21 +13,47 @@ class HooksTest < MiniTest::Test
         config.intercept_mechanism = :discard
         config.redirect_target = DEFAULT_INTERNAL_PHONE_NUMBER
       end
+
+      @action_texter_client = ActionTexter::TestClient.new
     end
 
     should "hook ActionTexter" do
+      ActionTexter::Client.setup("Test") # Excellent, no need to mock stuff up! Thank you ActionTexter!
       SmsSafe.hook!(:action_texter)
 
-      # Mock stuff
-      # Try to send a message
-      # Check that return is nil
-      # Check that nothing got sent
-      # Change configuration to redirect
-      # Try to send a message
-      # Check that return is appropriate
-      # Check that something got sent
-      # Check that the something that got sent was modified
+      # Try to send an external message
+      @action_texter_client.deliveries.clear
+      message = ActionTexter::Message.new(from: DEFAULT_INTERNAL_PHONE_NUMBER, to: EXTERNAL_PHONE_NUMBERS.first, text: "Foo", reference: "ref-1")
+      result = message.deliver
 
+      # Check that return is nil and that nothing got sent
+      assert_nil result
+      assert_equal 0, @action_texter_client.deliveries.length
+
+      # Change configuration to redirect
+      SmsSafe.configuration.intercept_mechanism = :redirect
+
+      # Try to send an external message
+      @action_texter_client.deliveries.clear
+      message = ActionTexter::Message.new(from: DEFAULT_INTERNAL_PHONE_NUMBER, to: EXTERNAL_PHONE_NUMBERS.first, text: "Foo", reference: "ref-1")
+      result = message.deliver
+
+      # Check that return is appropriate and that something got sent, redirected and with changed text
+      refute_nil result
+      assert_equal 1, @action_texter_client.deliveries.length
+      assert_equal DEFAULT_INTERNAL_PHONE_NUMBER, @action_texter_client.deliveries.last.to
+      assert_operator "Foo".length, :<, @action_texter_client.deliveries.last.text.length
+
+      # Try to send an internal message
+      @action_texter_client.deliveries.clear
+      message = ActionTexter::Message.new(from: DEFAULT_INTERNAL_PHONE_NUMBER, to: INTERNAL_PHONE_NUMBERS.last, text: "Foo", reference: "ref-1")
+      result = message.deliver
+
+      # Check that it got delivered, unchanged
+      refute_nil result
+      assert_equal 1, @action_texter_client.deliveries.length
+      assert_equal INTERNAL_PHONE_NUMBERS.last, @action_texter_client.deliveries.last.to
+      assert_equal "Foo", @action_texter_client.deliveries.last.text
     end
 
     should "hook Nexmo" do
